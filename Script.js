89a1f2e4c9e15fd20f71e911e02a369a8f1bc12c
Shih Feng Lin <noahleft@gmail.com>
2015-02-01 02:37:10 +0800

    simple structure (initial commit)

---
+++ Script.js
@@ -0,0 +1,6 @@
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='vendor.cgi'>Vendor</a></li>" +
+"<li><a href='about.html'>About</a></li>" +
+"</ul>";
---
+++ cgi-bin/vendor.cgi
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python3
+# -*- coding: UTF-8 -*-
+
+print("Content-type: text/html",end='\n\n')
+
+
+print("<html>Hello! </html>")
+
+
+
+
