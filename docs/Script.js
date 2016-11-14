233d11f96643406bc67eeae5282ee8f0742518ea
ShihFengLin <noahleft@gmail.com>
2016-11-15 00:23:40 +0800

    fix script js

--- docs/Script.js
+++ docs/Script.js
@@ -1,8 +1,8 @@
 document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
-"<li><a href='../index.html'>首頁</a></li>" +
-"<li><a href='../vendor.html'>廠商</a></li>" +
-"<li><a href='../product.html'>品項</a></li>" +
-"<li><a href='../record.html'>表單</a></li>" +
-"<li><a href='../about.html'>聯絡</a></li>" +
+"<li><a href='index.html'>首頁</a></li>" +
+"<li><a href='vendor.html'>廠商</a></li>" +
+"<li><a href='product.html'>品項</a></li>" +
+"<li><a href='record.html'>表單</a></li>" +
+"<li><a href='about.html'>聯絡</a></li>" +
 "</ul>";
